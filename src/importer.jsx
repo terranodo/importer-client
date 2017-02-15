25df6db9c4e9a6be9b9a0d32cad8f50c5f126d19
Mila Frerichs <mila.frerichs@gmail.com>
2017-02-15 10:41:36 -0800

    Use dropzone to add field to upload files

--- src/importer.jsx
+++ src/importer.jsx
@@ -3,14 +3,24 @@ global.React = React;
 import ReactDOM from 'react-dom';
 global.ReactDOM = ReactDOM;
 
+import Dropzone from 'react-dropzone';
+import {uploadFiles} from './services/geonode';
+
 class Importer extends React.Component {
   constructor(props) {
     super(props);
   }
+  onDrop(files) {
+    console.log('Received files: ', files);
+    let server = 'http://52.37.73.154/';
+    uploadFiles(server, files);
+  }
   render() {
     return (
       <div>
-        Test
+        <Dropzone onDrop={this.onDrop}>
+          <div>Try dropping some files here, or click to select files to upload.</div>
+        </Dropzone>
       </div>
     )
   }
