6409397a8908fcb35668f393d3bfc20f0fed90ae
Mila Frerichs <mila.frerichs@gmail.com>
2017-02-27 09:00:32 +0100

    Use step and new uploader component for the app

--- src/importer.jsx
+++ src/importer.jsx
@@ -3,24 +3,38 @@ global.React = React;
 import ReactDOM from 'react-dom';
 global.ReactDOM = ReactDOM;
 
-import Dropzone from 'react-dropzone';
-import {uploadFiles} from './services/geonode';
+import UploaderLink from './components/uploadLink';
+import Step from './components/step';
 
 class Importer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      step: 1,
+      steps: 2
+    };
   }
-  onDrop(files) {
-    console.log('Received files: ', files);
-    let server = 'http://52.37.73.154/';
-    uploadFiles(server, files);
+  next() {
+    this.setState({
+      step: this.state.step+1
+    })
+  }
+  prev() {
+    this.setState({
+      step: this.state.step-1
+    })
   }
   render() {
+    let stepElem;
+    if(this.state.step === 1) {
+        stepElem = (<UploaderLink />)
+    }
+    if(this.state.step === 2) {
+        stepElem = (<div>Test</div>)
+    }
     return (
       <div>
-        <Dropzone onDrop={this.onDrop}>
-          <div>Try dropping some files here, or click to select files to upload.</div>
-        </Dropzone>
+        <Step step={this.state.step} steps={this.state.steps} prev={this.prev.bind(this)} next={this.next.bind(this)}>{stepElem}</Step>
       </div>
     )
   }
