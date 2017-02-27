665bf5e7d4528a8a3ea486fe53569015751cfbbd
Mila Frerichs <mila.frerichs@gmail.com>
2017-02-27 08:57:57 +0100

    Add redux to the app

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
 import Importer from './importer.jsx'
-ReactDOM.render(<Importer/>, document.getElementById('main'));
+import configureStore from './configureStore.js';
+
+import {setServerUrl} from './state/server/actions';
+
+const store = configureStore();
+store.dispatch(setServerUrl('http://importer.terranodo.io/'));
+ReactDOM.render(<Provider store={store}><Importer/></Provider>, document.getElementById('main'));
--- tests/importer.test.js
+++ tests/importer.test.js
@@ -1,14 +1,18 @@
 import {should} from 'chai';
+import {Provider} from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 should();
 
 import ReactTestUtils from 'react-addons-test-utils';
 
 import Importer from '../src/importer.jsx';
 
+const mockStore = configureMockStore();
+const store = mockStore();
 
 describe('Importer', () => {
 	it('exists', () => {
-    const importerComponent = ReactTestUtils.renderIntoDocument(<Importer />);
+    const importerComponent = ReactTestUtils.renderIntoDocument(<Provider store={store}><Importer /></Provider>);
     ReactTestUtils.isCompositeComponent(importerComponent).should.be.true;
 	});
 });
