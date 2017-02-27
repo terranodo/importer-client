0257730da94a7eb912e79ebc07fa5ee704ea62c9
milafrerichs <mila.frerichs@gmail.com>
2017-02-27 17:48:41 +0100

    Allow hiding of next and prev buttons

--- src/components/step.jsx
+++ src/components/step.jsx
@@ -6,7 +6,11 @@ export default class Step extends React.PureComponent {
     steps: React.PropTypes.number.isRequired,
     next: React.PropTypes.func.isRequired,
     prev: React.PropTypes.func.isRequired,
+    showButons: React.PropTypes.bool,
     children: React.PropTypes.element
+  }
+  static defaultProps = {
+    showButtons: true
   }
   constructor(props) {
     super(props);
@@ -21,15 +25,17 @@ export default class Step extends React.PureComponent {
     let className = 'step-'+this.props.step;
     let nextStepclassName = 'next-step-'+this.props.step;
     let nextStep,prevStep = undefined;
-    if(this.props.step < this.props.steps) {
-      nextStep = (<div className={nextStepclassName}>
-          <button onClick={this._handleNext.bind(this)}>Next</button>
-        </div>);
-    }
-    if(this.props.step > 1) {
-      prevStep = (<div className={nextStepclassName}>
-          <button onClick={this._handlePrev.bind(this)}>Prev</button>
-        </div>);
+    if(this.props.showButtons) {
+      if(this.props.step < this.props.steps) {
+        nextStep = (<div className={nextStepclassName}>
+            <button onClick={this._handleNext.bind(this)}>Next</button>
+          </div>);
+      }
+      if(this.props.step > 1) {
+        prevStep = (<div className={nextStepclassName}>
+            <button onClick={this._handlePrev.bind(this)}>Prev</button>
+          </div>);
+      }
     }
     return (
       <div className={className}>
