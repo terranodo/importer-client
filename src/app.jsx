b8d7e935be1665f00b7202d0f2a6a80382d2c084
milafrerichs <mila.frerichs@gmail.com>
2017-03-21 16:39:01 +0100

    Use Material-UI and add theme

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import ImporterLink from './components/importerLink'
 import configureStore from './configureStore';
@@ -9,4 +11,24 @@ import {setServerUrl} from './state/server/actions';
 
 const store = configureStore();
 store.dispatch(setServerUrl('http://importer.terranodo.io/'));
-ReactDOM.render(<Provider store={store}><ImporterLink /></Provider>, document.getElementById('main'));
+
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: "#eb6b00",
+    primary2Color: "#eb6b00",
+  }
+});
+
+const App = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+		<Provider store={store}>
+			<ImporterLink/>
+		</Provider>
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('main')
+);
