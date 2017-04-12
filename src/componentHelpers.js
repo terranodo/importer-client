d47dc610b65665b9b2d691b9bc6edad2d9841028
milafrerichs <mila.frerichs@gmail.com>
2017-04-12 21:07:48 +0200

    Add label to fields

--- src/componentHelpers.js
+++ src/componentHelpers.js
@@ -2,17 +2,19 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 
-export const textFieldForKey = (keyName, value, changeCallback) => {
+export const textFieldForKey = (keyName, value, changeCallback, label='') => {
   return (<TextField
+          floatingLabelText={label}
           key={keyName}
           name={keyName}
           value={value}
           onChange={(event, newValue) => { changeCallback(keyName, newValue)}}/>
         )
 }
-export const selectFieldForKey = (keyName, items, value, changeCallback) => {
+export const selectFieldForKey = (keyName, items, value, changeCallback, label='') => {
   return (<SelectField
     key={keyName}
+    floatingLabelText={label}
     onChange={(event, index, newValue) => { changeCallback(keyName, newValue)}}
     value={value}
     >
